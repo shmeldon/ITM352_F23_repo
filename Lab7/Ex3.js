16d3ea3f505102b113966315cdbeb435d89377a6
Shmeldon <smka@hawaii.edu>
2023-10-05 03:36:07 -1000

    lab7 done

--- Lab7/Ex3.js
+++ Lab7/Ex3.js
@@ -1,16 +1,20 @@
 require("./products_data.js");
 
-let number_products = 5;
+let number_products = 5; // this variable may become unnecessary
 let product_number = 1;
 
-for(let product_number = 0 < number_product, product_number++) {
-    if (product_number > number_products * 0.25 && product_number < number_products * 0.75) {
-        console.log(`${eval('name' + product_number)} is sold out!`);
-        continue;
-    }
-    console.log(`${product_number}. ${eval('name' + product_number)}`);
-    if (product_number > number_products / 2) {
-        process.exit(1);
-    }
+// Only the condition in the for loop is changed
+for (
+  let product_number = 1;
+  eval("typeof name" + product_number) != "undefined";
+  product_number++
+) {
+  if (
+    product_number > number_products * 0.25 &&
+    product_number < number_products * 0.75
+  ) {
+    console.log(`${eval("name" + product_number)} is sold out!`);
+    continue;
+  }
+  console.log(`${product_number}. ${eval("name" + product_number)}`);
 }
-console.log('That\'s all we have!')
--- Lab7/products_data.js
+++ Lab7/products_data.js
@@ -1,21 +1,29 @@
 name1 = "HTC";
-price1 = 40.00;
+price1 = 40.0;
 image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
 
 name2 = "Apple";
-price2 = 75.00;
-image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+price2 = 75.0;
+image2 =
+  "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
 
 name3 = "Nokia";
-price3 = 35.00;
+price3 = 35.0;
 image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
 
 name4 = "Samsung";
-price4 = 45.00;
-image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+price4 = 45.0;
+image4 =
+  "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
 
 name5 = "Blackberry";
-price5 = 10.00;
-image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+price5 = 10.0;
+image5 =
+  "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+
+name6 = "Google";
+price6 = 100.0;
+image6 =
+  "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
 
 console.log("Product variables loaded...");
