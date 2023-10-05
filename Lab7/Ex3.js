1d823fffd390b2b700c2ef80185653532e699f4e
Shmeldon <smka@hawaii.edu>
2023-10-05 10:41:43 -1000

    Ex3.js fix

--- Lab7/Ex3.js
+++ Lab7/Ex3.js
@@ -1,14 +1,17 @@
 require("./products_data.js");
 
-let number_products = 5; // this variable may become unnecessary
+let number_products = 5; 
 let product_number = 1;
 
-// Only the condition in the for loop is changed
 for (
   let product_number = 1;
   eval("typeof name" + product_number) != "undefined";
   product_number++
 ) {
+ /*   if (product_number > number_products / 2) {
+        console.log("Don't ask for anything else!")
+        process.exit(1)
+    }
   if (
     product_number > number_products * 0.25 &&
     product_number < number_products * 0.75
@@ -16,5 +19,7 @@ for (
     console.log(`${eval("name" + product_number)} is sold out!`);
     continue;
   }
+  */
   console.log(`${product_number}. ${eval("name" + product_number)}`);
 }
+console.log('That\'s all!');
