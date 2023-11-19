a9e67ba7c3282e6613b02724503b516174652af7
Shmeldon <smka@hawaii.edu>
2023-11-18 19:56:19 -1000

    test

--- Aribal_Sheldon_Assignment1/server.js
+++ Aribal_Sheldon_Assignment1/server.js
@@ -3,6 +3,20 @@ const app = express();
 
 const products_array = require(__dirname + '/products.json');
 
+// Function to check if the quantities entered are whole numbers, negative values, and/or a number and not a string; Taken from labs
+function isNonNegInt(quantities, returnErrors) {
+    errors = []; // assume no errors at first
+    if (Number(quantities) != quantities) errors.push(' Not a number'); // Check if string is a number
+    if (quantities < 0) errors.push(' Negative value'); // Check to see if it is a non-negative value
+    if (parseInt(quantities) != quantities) errors.push(' Not an integer'); // Check to see if it is an integer
+ 
+    // Determine the return value and store it in 'result', then return that result. 
+    let result = returnErrors ? errors : (errors.length == 0);
+    return (result);
+ };
+ 
+//routing
+
 app.use(express.urlencoded({ extended: true }));
 
 app.all('*', function (request, response, next) {
@@ -12,7 +26,7 @@ app.all('*', function (request, response, next) {
 
 const products = require(__dirname + '/products.json');
 
-app.get("/product_data.js", function (request, response, next) {
+app.get("/products.js", function (request, response, next) {
    response.type('.js');
    let products_str = `var products = ${JSON.stringify(products)};`;
    response.send(products_str);
