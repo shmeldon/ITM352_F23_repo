da66cbedec5013441d0d7b38e4397a60a69a65bc
Shmeldon <smka@hawaii.edu>
2023-11-18 20:04:02 -1000

    asdf

---
+++ Aribal_Sheldon_Assignment1/products.js
@@ -0,0 +1,41 @@
+let products = [
+    {
+        "name": "Custom Pearl Necklace",
+        "price": 100.00,
+        "quantity_available": 20,
+        "image": "https://github.com/shmeldon/ITM352_F23_repo/blob/main/Aribal_Sheldon_Assignment1/public/images/pear_necklace.JPG?raw=true"
+    },
+    {
+        "name": "Custom Graphic Hoodie",
+        "price": 100.00,
+        "quantity_available": 5,
+        "image": "https://github.com/shmeldon/ITM352_F23_repo/blob/main/Aribal_Sheldon_Assignment1/public/images/custom_hoodie.JPG?raw=true"
+    },
+    {
+        "name": "Kith Mesh Shorts Grey",
+        "price": 60.00,
+        "quantity_available": 25,
+        "image": "https://github.com/shmeldon/ITM352_F23_repo/blob/main/Aribal_Sheldon_Assignment1/public/images/kith_shorts.JPG?raw=true"
+    },
+    {
+        "name": "Nike Mesh Shorts Black",
+        "price": 60.00,
+        "quantity_available": 25,
+        "image": "https://github.com/shmeldon/ITM352_F23_repo/blob/main/Aribal_Sheldon_Assignment1/public/images/nike_shorts.jpg"
+    },
+    {
+        "name": "Stussy International Tee",
+        "price": 75.00,
+        "quantity_available": 40,
+        "image": "https://github.com/shmeldon/ITM352_F23_repo/blob/main/Aribal_Sheldon_Assignment1/public/images/stussy_tee.jpg?raw=true"
+    },
+    {
+        "name": "Supreme Flannel Green",
+        "price": 120.00,
+        "quantity_available": 25,
+        "image": "https://github.com/shmeldon/ITM352_F23_repo/blob/main/Aribal_Sheldon_Assignment1/public/images/supreme_flannel.JPG?raw=true"
+    }
+];
+
+// Export the products array if needed in other modules
+// module.exports = products; // Uncomment this line if you're using Node.js
